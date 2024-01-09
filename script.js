fda140cb82efa85f0559b91f8e903784c94d113c
dpena1489 <150938385+dpena1489@users.noreply.github.com>
2024-01-08 20:13:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  return;
+};
+
+// Add event listener to generate the button
+generateBtn.addEventListener("click", writePassword);
+
+// Var for characters
+var uChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lChars = "abcdefghijklmnopqrstuvwxyz";
+var nChars = "0123456789";
+var sChars = "~!@#$%^&*?";
+
+// generate PW function code below
+function generatePassword() {
+  var actualPassword = "";
+  
+  alert("You will be presented with the following prompts to select criteria for generating a password.");
+
+  // choices of password length between 8-128
+  var pwdLength = prompt("Choose a number between 8 and 128. This choice will be the length of the password.", );
+  if (pwdLength != null) {
+    if (pwdLength >= 8 && pwdLength <= 128) {
+
+      // choices of Lowercase, Uppercase, Numerals, Special Character
+      alert("In the following prompts, at least one choice of lowercase, uppercase, numeric, and/or special characters must be confirmed.");
+
+      var chooser = false;
+      var charChoices = "";
+
+      var uCase = confirm("Would you like your password to contain uppercase characters?");
+      if (uCase === true) {
+        chooser = true;
+        charChoices += uChars; 
+      }
+
+      var lCase = confirm("Would you like your password to contain lowercase characters?");
+      if (lCase === true) {
+        chooser = true;
+        charChoices += lChars;
+      }
+
+      var numChoice = confirm("Would you like your password to contain numeric characters?");
+      if (numChoice === true) {
+        chooser = true;
+        charChoices += nChars;
+      }
+
+      var specChar = confirm("Would you like your password to contain special characters?");
+      if (specChar === true) {
+        chooser = true;
+        charChoices += sChars;
+      }
+
+      if (chooser === false) {
+        alert("At least one choice of lowercase, uppercase, numeric, and/or special characters must be confirmed.");
+      } else {
+
+        // PW generation code
+        for (i = 1; i < pwdLength ; i++ ) {
+          actualPassword += charChoices[Math.floor(Math.random()*charChoices.length)];
+        };
+      };
+
+    } else {
+      alert("Please enter a number between 8 and 128")
+    };
+
+  };
+
+  return actualPassword;
+}
